5ee8afb8a8584741bbdec9b903661b4de2109991
portatil-V15 <ikerperezmata03@gamil.com>
2025-04-23 13:58:39 +0200

    Sixth commit: - Cambio de alojamiento bd - Landing page

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -6,9 +6,9 @@ import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabases() {
     val database = Database.connect(
-        url = "jdbc:mysql://localhost:3306/testDB",
+        url = "jdbc:mysql://tunelinforonda.ddns.net:3306/testDB",
         driver = "com.mysql.cj.jdbc.Driver",
-        user = "root",
-        password = "1234"
+        user = "Practicas",
+        password = "admin1234*"
     )
 }
