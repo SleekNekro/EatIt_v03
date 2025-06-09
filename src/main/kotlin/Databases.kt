4cbe23ca94632f5d09c4fc112dd8ece7c98af2ea
Casa <ikerperezmata03@gmail.com>
2025-06-09 21:50:56 +0200

    v5

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -5,11 +5,18 @@ import org.jetbrains.exposed.sql.Database
 
 // mysql://root:vtmQNCsHGrZQExcofMOHJNHZiJtvipbq@caboose.proxy.rlwy.net:49981/railway
 fun Application.configureDatabases() {
+    // Opción 1: Usar MYSQL_URL directamente (si Railway lo define)
+    val databaseUrl = System.getenv("MYSQL_URL")
+        ?: "jdbc:mysql://${System.getenv("MYSQLHOST")}:${System.getenv("MYSQLPORT")}/${System.getenv("MYSQLDATABASE")}?serverTimezone=UTC&useSSL=false"
+    val dbUser = System.getenv("MYSQLUSER") ?: error("Falta MYSQLUSER")
+    val dbPassword = System.getenv("MYSQLPASSWORD") ?: error("Falta MYSQLPASSWORD")
+
+    // Usamos el driver de MySQL
     val database = Database.connect(
-        url = System.getenv("DATABASE_URL"),
-        driver = System.getenv("DATABASE_DRIVER"),
-        user = System.getenv("DATABASE_USER"),
-        password = System.getenv("DATABASE_PASSWORD")
+        url = databaseUrl,
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = dbUser,
+        password = dbPassword
     )
-    println("$database".toString())
+    println("Database connected: $database")
 }
