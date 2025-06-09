6686b9e395526e5bb7e1144995b03d2b726374a3
Casa <ikerperezmata03@gmail.com>
2025-06-09 16:15:20 +0200

    16:15 v2

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -3,12 +3,12 @@ package com.github.SleekNekro
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 
-
+// mysql://root:vtmQNCsHGrZQExcofMOHJNHZiJtvipbq@caboose.proxy.rlwy.net:49981/railway
 fun Application.configureDatabases() {
     val database = Database.connect(
-        url = "jdbc:mysql://localhost:3306/eatit",
+        url = "jdbc:mysql://caboose.proxy.rlwy.net:49981/eatit",
         driver = "com.mysql.cj.jdbc.Driver",
         user = "root",
-        password = "1234"
+        password = "vtmQNCsHGrZQExcofMOHJNHZiJtvipbq"
     )
 }
